20bdcf2c4eb48a592fafe83a6bb1a7d8b1ec41c5
Rohan <rohankaradigudd@gmail.com>
2026-04-20 13:39:11 +0530

    New Release of Portfolio

--- script.js
+++ script.js
@@ -194,7 +194,7 @@ if (contactForm) {
         submitBtn.textContent = 'Sending...';
 
         try {
-            const response = await fetch('http://localhost:8001/api/contact', {
+            const response = await fetch('https://portfolio-backend-l3y2.onrender.com/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
